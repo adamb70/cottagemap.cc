42dd92ddd5c7cee9f81913197b4c8b70c423917c
adamb70 <adam.boaler@hotmail.com>
2019-07-30 11:54:49 +0100

    - Display times for 1 hour/minute/etc

--- static/cottagemap.js
+++ static/cottagemap.js
@@ -11,22 +11,32 @@
 		let interval = Math.floor(seconds / 31536000);
 		if (interval > 1) {
 			return interval + " years";
+		} else if (interval === 1) {
+			return interval + " year";
 		}
 		interval = Math.floor(seconds / 2592000);
 		if (interval > 1) {
 			return interval + " months";
+		} else if (interval === 1) {
+			return interval + " month";
 		}
 		interval = Math.floor(seconds / 86400);
 		if (interval > 1) {
 			return interval + " days";
+		} else if (interval === 1) {
+			return interval + " day";
 		}
 		interval = Math.floor(seconds / 3600);
 		if (interval > 1) {
 			return interval + " hours";
+		} else if (interval === 1) {
+			return interval + " hour";
 		}
 		interval = Math.floor(seconds / 60);
 		if (interval > 1) {
 			return interval + " minutes";
+		} else if (interval === 1) {
+			return interval + " minute";
 		}
 		return Math.floor(seconds) + " seconds";
 	}
