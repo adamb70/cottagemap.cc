a9d01d215413f457c585448fa3dd59df842ce3c1
adamb70 <adam.boaler@hotmail.com>
2020-10-26 09:14:42 +0000

    - Temporarily disable crawler task

--- run_app.py
+++ run_app.py
@@ -25,7 +25,7 @@ def keep_awake():
 def start_scheduler():
     # Attention: you cannot use a blocking scheduler here as that will block the script from proceeding.
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=crawler_task, trigger=CronTrigger(hour=1, day_of_week='mon'))
+    #scheduler.add_job(func=crawler_task, trigger=CronTrigger(hour=1, day_of_week='mon'))
     scheduler.add_job(func=keep_awake, trigger=CronTrigger.from_crontab('*/15 * * * *'))
     scheduler.start()
 
